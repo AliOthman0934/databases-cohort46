ec5978e90032fe03578e8b20dc49aab64a32ddeb
Ali Othman <alioth840@gmail.com>
2024-02-21 21:35:05 +0100

    I implemented my mentor feedback

--- Week1/meetupDatabase/meetupDatabase.js
+++ Week1/meetupDatabase/meetupDatabase.js
@@ -10,46 +10,47 @@ const db = mysql.createConnection({
 
 
 db.connect((err) => {
-    if (err) throw err;
-
-    console.log('Connected to MySQL');
-
-    db.query('DROP DATABASE IF EXISTS meetup', (err) => {
+    try {
         if (err) throw err;
-        console.log('Database dropped or did not exist')
-    });
 
+        console.log('Connected to MySQL');
 
-    db.query('CREATE DATABASE IF NOT EXISTS meetup', (err) => {
-        if (err) throw err;
-        console.log('Database created');
-
-
-        db.query('USE meetup', (err) => {
+        db.query('DROP DATABASE IF EXISTS meetup', (err) => {
             if (err) throw err;
+            console.log('Database dropped or did not exist')
+        });
 
 
-            db.query(`CREATE TABLE IF NOT EXISTS Invitee (
+        db.query('CREATE DATABASE IF NOT EXISTS meetup', (err) => {
+            if (err) throw err;
+            console.log('Database created');
+
+
+            db.query('USE meetup', (err) => {
+                if (err) throw err;
+
+
+                db.query(`CREATE TABLE IF NOT EXISTS Invitee (
             invitee_no INT AUTO_INCREMENT PRIMARY KEY,
             invitee_name VARCHAR(255),
             invited_by VARCHAR(255)
             )
         `, (err) => {
-                if (err) throw err;
-                console.log('Invitee table created ');
+                    if (err) throw err;
+                    console.log('Invitee table created ');
 
 
-                db.query(`CREATE TABLE IF NOT EXISTS Room (
+                    db.query(`CREATE TABLE IF NOT EXISTS Room (
             room_no INT AUTO_INCREMENT PRIMARY KEY,
             room_name VARCHAR(255),
             floor_number INT
             )
         `, (err) => {
-                    if (err) throw err;
-                    console.log('Room table created ');
+                        if (err) throw err;
+                        console.log('Room table created ');
 
 
-                    db.query(`CREATE TABLE IF NOT EXISTS Meeting (
+                        db.query(`CREATE TABLE IF NOT EXISTS Meeting (
                 meeting_no INT AUTO_INCREMENT PRIMARY KEY,
                 meeting_title VARCHAR(255),
                 starting_time DATETIME,
@@ -57,41 +58,45 @@ db.connect((err) => {
                 room_no INT,
                 FOREIGN KEY (room_no) REFERENCES Room(room_no)
             )
-            `, (err) => {
-                        if (err) throw err;
-                        console.log('Meeting table created');
+        `, (err) => {
+                            if (err) throw err;
+                            console.log('Meeting table created');
 
-                        insertData();
+                            insertData();
+                        });
                     });
                 });
             });
         });
-    });
+    } catch (error) {
+        throw error
+    }
 });
 
 function insertData() {
+    try {
 
-    db.query(`INSERT INTO Invitee (invitee_name, invited_by) VALUES
+        db.query(`INSERT INTO Invitee (invitee_name, invited_by) VALUES
         ('Ali', 'Hann'),
         ('Carmen', 'Gab'),
         ('Noor', 'Ahmed'),
         ('Salam', 'Amar'),
         ('Islam', 'Salih')
     `, (err) => {
-        if (err) throw err;
-        console.log('All the meeting are scheduled');
+            if (err) throw err;
+            console.log('All the meeting are scheduled');
 
-        db.query(`INSERT INTO Room (room_name, floor_number) VALUES
+            db.query(`INSERT INTO Room (room_name, floor_number) VALUES
         ('Room1', 1),
         ('Room2', 2),
         ('Room3', 3),
         ('Room4', 4),
         ('Room5', 5)
     `, (err) => {
-            if (err) throw err;
-            console.log('All the rooms are booked for the meeting');
+                if (err) throw err;
+                console.log('All the rooms are booked for the meeting');
 
-            db.query(`
+                db.query(`
         INSERT INTO Meeting (meeting_title, starting_time, ending_time, room_no) VALUES
         ('Meeting1', '2024-02-17 10:00:00', '2024-02-17 11:30:00', 1),
         ('Meeting2', '2024-02-18 14:00:00', '2024-02-18 15:30:00', 2),
@@ -99,14 +104,17 @@ function insertData() {
         ('Meeting4', '2024-02-20 13:00:00', '2024-02-20 14:30:00', 4),
         ('Meeting5', '2024-02-21 11:00:00', '2024-02-21 12:30:00', 5)
         `, (err) => {
-                if (err) throw err;
-                console.log('All the meeting are scheduled');
-
-                db.end((err) => {
                     if (err) throw err;
-                    console.log('Connection closed');
+                    console.log('All the meeting are scheduled');
+
+                    db.end((err) => {
+                        if (err) throw err;
+                        console.log('Connection closed');
+                    });
                 });
             });
         });
-    });
+    } catch (error) {
+        throw error
+    }
 }
